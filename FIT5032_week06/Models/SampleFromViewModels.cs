7f332532ca30e49d7a70c9e393f61be5affc251c
DataGen64 <bkri0003@student.monash.edu>
2019-10-08 19:05:19 +1100

    first commit

---
+++ FIT5032_week06/Models/SampleFromViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FIT5032_week06.Models
+{
+    public class SampleFromViewModels
+    {
+    }
+
+    public class FromOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
